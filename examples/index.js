7b6537b0886faae5a5f6f204274b5bd2e8b2644c
Олег Дутченко <dutchenko.o.dev@gmail.com>
2020-04-14 01:37:44 +0300

    0.0.5-alpha

---
+++ examples/index.js
@@ -0,0 +1,7 @@
+(function () {
+	'use strict';
+
+	console.log('Hello');
+
+}());
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello');
+import { ILayout } from './types';
+
+export default (...layouts: ILayout[]) => {
+	const keyboard: {[p: string]: string[] }= {};
+
+	const length = layouts.length;
+	for (let i = 0; i < length; i++) {
+		const layout = layouts[i];
+		for (const key in layout) {
+			if (layout.hasOwnProperty(key)) {
+				if (!keyboard.hasOwnProperty(key)) {
+					keyboard[key] = [];
+				}
+				keyboard[key].push(layout[key]);
+			}
+		}
+	}
+
+	return (original: string): string[] => {
+		return [original];
+	}
+}
